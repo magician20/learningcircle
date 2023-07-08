6538426e27762410ae9cee4b333dc426d9065f2c
magician20 <am.elsantawy.cs@gmail.com>
2023-07-08 17:14:46 +0300

    start to update the gateway

--- greetings-api/src/main/java/com/magician/greetingsapi/GreetingController.java
+++ greetings-api/src/main/java/com/magician/greetingsapi/GreetingController.java
@@ -5,12 +5,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
+@RequestMapping(path = "/v1")
 public class GreetingController {
 
     @GetMapping("/greet")
